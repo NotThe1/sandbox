e5cfa94d04d0ef638ed854234908b8f5df623f8d
Frank Martyn <FrankMartyn1945@gmail.com>
2016-11-14 12:04:57 -0500

    Moved hexEdit to project VM

--- sandbox/src/hexEdit/HexSourceChangeListener.java
+++
@@ -1,8 +0,0 @@
-package hexEdit;
-
-import java.util.EventListener;
-
-
-public interface HexSourceChangeListener extends EventListener {
-	public void dataChanged(HexSourceChangeEvent hexSourceChangeEvent);
-}//HexSourceChangeListener
