b0189df55067a475ebe4f9d6be65c627d4be2020
Frank Martyn <FrankMartyn1945@gmail.com>
2017-01-18 17:11:20 -0500

    clean up

--- sandbox/src/box2/TestMenus.java
+++ sandbox/src/box2/TestMenus.java
@@ -10,6 +10,10 @@ import java.awt.Insets;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.FocusAdapter;
+import java.awt.event.FocusEvent;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.io.BufferedReader;
@@ -19,8 +23,15 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.HashMap;
 
+import javax.swing.AbstractAction;
 import javax.swing.AbstractButton;
+import javax.swing.Action;
+import javax.swing.ActionMap;
+import javax.swing.DefaultComboBoxModel;
+import javax.swing.InputMap;
+import javax.swing.JButton;
 import javax.swing.JCheckBoxMenuItem;
+import javax.swing.JComboBox;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -28,14 +39,16 @@ import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JOptionPane;
+import javax.swing.JPopupMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JSpinner;
 import javax.swing.JTextArea;
 import javax.swing.JTextField;
+import javax.swing.KeyStroke;
 import javax.swing.SpinnerNumberModel;
 import javax.swing.filechooser.FileNameExtensionFilter;
 
-public class TestMenus {
+public class TestMenus implements ActionListener{
 
 	private JFrame frame;
 	private JMenu mnuFile;
@@ -45,6 +58,10 @@ public class TestMenus {
 	private HashMap<File, String> listings;
 	private JTextField txtFormat;
 	private JSpinner spinValue;
+	DefaultComboBoxModel<String> fred = new DefaultComboBoxModel<String>(new String[] { "one", "two", "three" });
+	private JComboBox comboBox;
+	
+	JPopupMenu popupMenu = new JPopupMenu();
 
 	/**
 	 * Launch the application.
@@ -108,29 +125,69 @@ public class TestMenus {
 	private void appInit() {
 		// fileList = new HashMap<File, Point>();
 		// listings = new HashMap<File, String>();
-		SpinnerNumberModel n = (SpinnerNumberModel) spinValue.getModel();
 		
+		SpinnerNumberModel n = (SpinnerNumberModel) spinValue.getModel();
+
 		Component[] components = spinValue.getComponents();
 		int i = 0;
 		for (Component c : components) {
 			if (c instanceof AbstractButton) {
-//				c.setVisible(false);
-				System.out.printf("[TestMenus:appInit]component[%d]: %s%n",i++, c.toString());
-//				c = null;
-//				int nextNumber = 0;//(int) n.getNextValue();
-//				int number = (int) n.getNumber();
-//				System.out.printf("[TestMenus:appInit]Model - number : %d, next number: %d%n", number,nextNumber);
-				
-//				int nextNumberS = (int) spinValue.getValue();
-//				int numberS = ((Integer) spinValue.getNextValue())==null?(int) n.getMaximum():(int) spinValue.getNextValue();
-//				int numberS = ((Integer) spinValue.getNextValue());
-//				System.out.printf("[TestMenus:appInit]Spinner - number : %d, next number: %d%n", numberS,nextNumberS);
+				// c.setVisible(false);
+				// System.out.printf("[TestMenus:appInit]component[%d]: %s%n",i++, c.toString());
+				// c = null;
+				// int nextNumber = 0;//(int) n.getNextValue();
+				// int number = (int) n.getNumber();
+				// System.out.printf("[TestMenus:appInit]Model - number : %d, next number: %d%n", number,nextNumber);
 
-			}//if
-		}//for
+				// int nextNumberS = (int) spinValue.getValue();
+				// int numberS = ((Integer) spinValue.getNextValue())==null?(int) n.getMaximum():(int)
+				// spinValue.getNextValue();
+				// int numberS = ((Integer) spinValue.getNextValue());
+				// System.out.printf("[TestMenus:appInit]Spinner - number : %d, next number: %d%n",
+				// numberS,nextNumberS);
+
+			} // if
+		} // for
 		int a = 0;
+		 String ACTION_KEY = "theAction";		
+			KeyStroke controlAlt7 = KeyStroke.getKeyStroke("control alt 7");
+			
+//			InputMap im = comboBox.getInputMap();			
+//			im.put(controlAlt7, ACTION_KEY);
+//			
+//			ActionMap am = comboBox.getActionMap();			
+//			am.put(ACTION_KEY, actionListener);
+//			
+//			InputMap im1 = txtLog.getInputMap();
+//			im1.put(controlAlt7, ACTION_KEY);
+//			
+//			ActionMap am1 = txtLog.getActionMap();
+//			am1.put(ACTION_KEY, actionListener);
+			
+			InputMap im3 = txtFormat.getInputMap();
+			im3.put(controlAlt7, ACTION_KEY);
+			
+			ActionMap am3 = txtFormat.getActionMap();
+			am3.put(ACTION_KEY, actionListener);
+			
+			JMenuItem firstItem = new JMenuItem("control alt 7");
+			firstItem.addActionListener(actionListener);
+			popupMenu.add(firstItem);
+			comboBox.setComponentPopupMenu(popupMenu);
+		
 
-	}
+		
+
+	}// appInit
+		Action actionListener = new AbstractAction() {
+			public void actionPerformed(ActionEvent actionEvent) {
+				System.out.println("Selected Index:        xxx");
+				
+				System.out.println("Selected Index: " + comboBox.getSelectedIndex());
+			}
+		};
+		private JTextArea txtLog;
+		private JButton btnDoSomething;
 
 	/**
 	 * Create the application.
@@ -151,7 +208,7 @@ public class TestMenus {
 		gridBagLayout.columnWidths = new int[] { 0, 100, 0, 0, 0 };
 		gridBagLayout.rowHeights = new int[] { 0, 0, 0, 0, 0, 0 };
 		gridBagLayout.columnWeights = new double[] { 0.0, 0.0, 0.0, 1.0, Double.MIN_VALUE };
-		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 1.0, 1.0, Double.MIN_VALUE };
+		gridBagLayout.rowWeights = new double[] { 0.0, 0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE };
 		frame.getContentPane().setLayout(gridBagLayout);
 
 		JLabel lblResult = new JLabel("Result");
@@ -193,9 +250,9 @@ public class TestMenus {
 		gbc_scrollPane.gridy = 2;
 		frame.getContentPane().add(scrollPane, gbc_scrollPane);
 
-		JTextArea txtLog = new JTextArea();
+		txtLog = new JTextArea();
 		txtLog.setText("One");
-		scrollPane.setViewportView(txtLog);
+		scrollPane.setViewportView(txtLog);		
 
 		spinValue = new JSpinner();
 		spinValue.setModel(new SpinnerNumberModel(1, 0, 1, 1));
@@ -206,6 +263,55 @@ public class TestMenus {
 		gbc_spinValue.gridx = 3;
 		gbc_spinValue.gridy = 2;
 		frame.getContentPane().add(spinValue, gbc_spinValue);
+
+		comboBox = new JComboBox(fred);
+		comboBox.addItemListener(new ItemListener() {
+			public void itemStateChanged(ItemEvent itemEvent) {
+				
+				if(fred.getIndexOf(itemEvent.getItem()) == -1){
+					fred.insertElementAt((String) itemEvent.getItem(), 0);
+//					comboBox.repaint();
+				}//if - not there
+				
+				itemEvent.getStateChange();
+				itemEvent.paramString();
+				itemEvent.getItem();
+				System.out.printf("[comboBox.itemStateChanged] getStateChange() %s%n",itemEvent.getStateChange());
+				System.out.printf("[comboBox.itemStateChanged] paramString() %s%n",itemEvent.paramString());
+				System.out.printf("[comboBox.itemStateChanged] getItem() %s%n",itemEvent.getItem());
+			}
+		});
+		comboBox.addFocusListener(new FocusAdapter() {
+			@Override
+			public void focusLost(FocusEvent arg0) {
+				Object object = comboBox.getSelectedItem();
+				if(fred.getIndexOf(object) == -1){
+					fred.insertElementAt((String) object, 0);
+					comboBox.repaint();
+				}//if - not there
+			}//focus Lost
+		});
+		comboBox.setEditable(true);
+		GridBagConstraints gbc_comboBox = new GridBagConstraints();
+		gbc_comboBox.insets = new Insets(0, 0, 5, 0);
+		gbc_comboBox.fill = GridBagConstraints.HORIZONTAL;
+		gbc_comboBox.gridx = 3;
+		gbc_comboBox.gridy = 3;
+		frame.getContentPane().add(comboBox, gbc_comboBox);
+
+		btnDoSomething = new JButton("do Something");
+		btnDoSomething.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				Object subject = comboBox.getSelectedItem();
+				int loc = fred.getIndexOf(subject);
+				System.out.printf("[btnDoSomething] subject: %s  loc: %d%n", subject, loc);
+				fred.insertElementAt((String) subject, 0);
+			}// actionPerformed
+		});
+		GridBagConstraints gbc_btnDoSomething = new GridBagConstraints();
+		gbc_btnDoSomething.gridx = 3;
+		gbc_btnDoSomething.gridy = 4;
+		frame.getContentPane().add(btnDoSomething, gbc_btnDoSomething);
 
 		// GridBagConstraints gbc_list = new GridBagConstraints();
 		// gbc_list.fill = GridBagConstraints.BOTH;
@@ -265,6 +371,12 @@ public class TestMenus {
 			}// actionPerformed
 		});
 		mnuFile.add(mnuFIleRemove);
+	}
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		System.out.printf("[actionPerformed]  %s%n", arg0.toString());
+		
 	}
 
 }
