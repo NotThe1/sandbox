66694c891243fd19465a265a122faafee358b69d
Frank Martyn <FrankMartyn1945@gmail.com>
2017-03-07 15:08:36 -0500

    played with disk utility problem with new disks

---
+++ sandbox/src/Misc/NewDisk.java
@@ -0,0 +1,86 @@
+package Misc;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
+import java.nio.channels.FileChannel;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+public class NewDisk {
+	public static String makeNewDisk() {
+
+		String fileExtension = "F3HD";
+		JFileChooser fc = new JFileChooser("C:\\Users\\admin\\VMdata\\Disks");
+		if (fc.showOpenDialog(null) == JFileChooser.CANCEL_OPTION) {
+			System.out.println("Bailed out of the open");
+			return null;
+		} // if
+
+		// File pickedFile = fc.getSelectedFile();
+
+		String targetRawAbsoluteFileName = fc.getSelectedFile().getAbsolutePath();
+		String[] fileNameComponents = targetRawAbsoluteFileName.split("\\.");
+		String targetAbsoluteFileName = fileNameComponents[0] + "." + fileExtension;
+
+		// File selectedFile = new File(targetAbsoluteFileName);
+		if (Files.exists(Paths.get(targetAbsoluteFileName))) {
+			if (JOptionPane.showConfirmDialog(null, "File already exists do you want to overwrite it?",
+					"YES - Continue, NO - Cancel", JOptionPane.YES_NO_OPTION) == JOptionPane.NO_OPTION) {
+				return null;
+			} else {
+				try {
+					Files.delete(Paths.get(targetAbsoluteFileName));
+//					Files.createFile(Paths.get(targetAbsoluteFileName));
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			} // inner if
+		} // if - file exists
+
+		try (FileChannel fileChannel = new RandomAccessFile(targetAbsoluteFileName, "rws").getChannel();) {
+			MappedByteBuffer disk = fileChannel.map(FileChannel.MapMode.READ_WRITE, 0, 51200);
+//
+//			ByteBuffer sector = ByteBuffer.allocate(512);
+//			int sectorCount = 0;
+//			while (disk.hasRemaining()) {
+//				sector = setUpBuffer(sector, sectorCount++);
+//				disk.put(sector);
+//			} // while
+
+			fileChannel.force(true);
+			fileChannel.close();
+			disk.force();
+
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		return targetAbsoluteFileName;
+	}// makeNewDisk
+
+	private static ByteBuffer setUpBuffer(ByteBuffer sector, int value) {
+		sector.clear();
+		// set value to be put into sector
+		Byte byteValue = (byte) 0x00; // default to null
+		Byte MTfileVlaue = (byte) 0xE5; // deleted file value
+		Byte workingValue;
+		while (sector.hasRemaining()) {
+			workingValue = ((sector.position() % 0x20) == 0) ? MTfileVlaue : byteValue;
+			sector.put(workingValue);
+		} // while
+		sector.flip();
+		return sector;
+	}// setUpBuffer
+
+}
--- sandbox/src/Misc/TempFiles.java
+++ sandbox/src/Misc/TempFiles.java
@@ -8,13 +8,17 @@ import java.awt.Insets;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.FilenameFilter;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
 import java.util.Map;
 import java.util.Set;
 import java.util.prefs.Preferences;
@@ -57,16 +61,16 @@ public class TempFiles {
 					window.frmTemplate.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
-				}//try
-			}//run
+				} // try
+			}// run
 		});
 	}// main
 
 	/* Standard Stuff */
-	
-	private void doBtnOne(){
+
+	private void doBtnOne() {
 		try {
-			oneFile =File.createTempFile(PREFIX, SUFFIX);
+			oneFile = File.createTempFile(PREFIX, SUFFIX);
 			File.createTempFile(PREFIX, SUFFIX);
 			File.createTempFile(PREFIX, SUFFIX);
 			File.createTempFile(PREFIX, SUFFIX);
@@ -75,99 +79,139 @@ public class TempFiles {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
-		
-	}//doBtnOne
-	
-	private void doBtnTwo(){
+
+	}// doBtnOne
+
+	private void doBtnTwo() {
 		doBtnOne();
-		String TEMP = "TEMP"; 
+		String TEMP = "TEMP";
 		String tempDir = System.getenv(TEMP);
 		textPane.setText("");
-		textPane.append(String.format("%-25s = %s%n%n", TEMP,tempDir));
-		
+		textPane.append(String.format("%-25s = %s%n%n", TEMP, tempDir));
+
 		File targetDir = new File(tempDir);
 		String xx = File.separator;
 		String[] tempFiles = targetDir.list(new PrefixFilter(PREFIX));
-		for(String tempFile:tempFiles){
-			textPane.append(String.format("%s%n", tempDir+ xx + tempFile));
-		}//for
-		
-		
-		for(String tempFile:tempFiles){
+		for (String tempFile : tempFiles) {
+			textPane.append(String.format("%s%n", tempDir + xx + tempFile));
+		} // for
+
+		for (String tempFile : tempFiles) {
 			Boolean result;
 			try {
-				result =Files.deleteIfExists(Paths.get(tempDir+ xx + tempFile));
-				textPane.append(String.format("%-10s,%s%n",result,tempDir+ xx + tempFile));
+				result = Files.deleteIfExists(Paths.get(tempDir + xx + tempFile));
+				textPane.append(String.format("%-10s,%s%n", result, tempDir + xx + tempFile));
 			} catch (IOException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
-			}//
-		}//for
-		
-	}//doBtnTwo
-	
-	private void doBtnThree(){
-		
-	}//doBtnThree
-	
-	private void doBtnFour(){
+			} //
+		} // for
+
+	}// doBtnTwo
+
+	private void doBtnThree() {
+		String fileName = NewDisk.makeNewDisk();
+		textPane.append(String.format("New disk file name: %s%n", fileName));
+		int counter = 0;
+		while (!Files.isWritable(Paths.get(fileName))){
+			try {
+				Thread.sleep(1000);
+			} catch (InterruptedException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			if(counter++ > 60){
+				break;
+			}
+			textPane.append(String.format("doButtonThree: counter = %d%n",counter));
+		}
+		updateDisk(Paths.get(fileName));
+	}// doBtnThree
+
+	private void updateDisk(Path path) {
+		File workDisk = null;
+		try {
+			workDisk = File.createTempFile("SandboxTempFiles", "." + "stuff");
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		Path target = path;
+		Path source = Paths.get(workDisk.getAbsolutePath());
+		try {
+			Files.copy(source, target, StandardCopyOption.REPLACE_EXISTING);
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		} // try
+	}// updateDisk
+
+	private void doBtnFour() {
 		textPane.setText("");
-		Map<String,String> envVars = System.getenv();
+		Map<String, String> envVars = System.getenv();
 		Set<String> keys = envVars.keySet();
-		for(String key:keys){
-			textPane.append(String.format("%-25s = %s%n", key,envVars.get(key)));
-		}//for
-		
-		textPane.append(String.format("%n%-25s = %s%n", "TEMP",System.getenv("TEMP")));
-		
-	}//doBtnFour
-	//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+		for (String key : keys) {
+			textPane.append(String.format("%-25s = %s%n", key, envVars.get(key)));
+		} // for
+
+		textPane.append(String.format("%n%-25s = %s%n", "TEMP", System.getenv("TEMP")));
+
+	}// doBtnFour
+		// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
 	class PrefixFilter implements FilenameFilter {
-		  String prefix;
+		String prefix;
 
-		  public PrefixFilter(String prefix) {
-		    this.prefix = prefix;
-		  }//Constructor
+		public PrefixFilter(String prefix) {
+			this.prefix = prefix;
+		}// Constructor
 
-		  public boolean accept(File dir, String name) {
-			  boolean yes = name.startsWith(prefix);
-			    return yes;
-			   // return name.endsWith(ext);
-		  }//accept
-		}//class PrefixFilter
-	//:::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-	
-	//---------------------------------------------------------
-	
-	private void doFileNew(){
-		
-	}//doFileNew
-	private void doFileOpen(){
-		
-	}//doFileOpen
-	private void doFileSave(){
-		
-	}//doFileSave
-	private void doFileSaveAs(){
-		
-	}//doFileSaveAs
-	private void doFilePrint(){
-		
-	}//doFilePrint
-	private void doFileExit(){
+		public boolean accept(File dir, String name) {
+			boolean yes = name.startsWith(prefix);
+			return yes;
+			// return name.endsWith(ext);
+		}// accept
+	}// class PrefixFilter
+	// :::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+
+	// ---------------------------------------------------------
+
+	private void doFileNew() {
+
+	}// doFileNew
+
+	private void doFileOpen() {
+
+	}// doFileOpen
+
+	private void doFileSave() {
+
+	}// doFileSave
+
+	private void doFileSaveAs() {
+
+	}// doFileSaveAs
+
+	private void doFilePrint() {
+
+	}// doFilePrint
+
+	private void doFileExit() {
 		appClose();
 		System.exit(0);
-	}//doFileExit
-	private void doEditCut(){
-		
-	}//doEditCut
-	private void doEditCopy(){
-		
-	}//doEditCopy
-	private void doEditPaste(){
-		
-	}//doEditPaste
+	}// doFileExit
+
+	private void doEditCut() {
+
+	}// doEditCut
+
+	private void doEditCopy() {
+
+	}// doEditCopy
+
+	private void doEditPaste() {
+
+	}// doEditPaste
 
 	private void appClose() {
 		Preferences myPrefs = Preferences.userNodeForPackage(TempFiles.class);
@@ -179,7 +223,7 @@ public class TempFiles {
 		myPrefs.putInt("LocY", point.y);
 		myPrefs.putInt("Divider", splitPane1.getDividerLocation());
 		myPrefs = null;
-	}//appClose
+	}// appClose
 
 	private void appInit() {
 		Preferences myPrefs = Preferences.userNodeForPackage(TempFiles.class);
@@ -208,7 +252,7 @@ public class TempFiles {
 		gridBagLayout.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
 		gridBagLayout.rowWeights = new double[] { 0.0, 1.0, 0.0, Double.MIN_VALUE };
 		frmTemplate.getContentPane().setLayout(gridBagLayout);
-		
+
 		JToolBar toolBar = new JToolBar();
 		toolBar.setFloatable(false);
 		GridBagConstraints gbc_toolBar = new GridBagConstraints();
@@ -217,8 +261,9 @@ public class TempFiles {
 		gbc_toolBar.gridx = 0;
 		gbc_toolBar.gridy = 0;
 		frmTemplate.getContentPane().add(toolBar, gbc_toolBar);
-		
+
 		btnOne = new JButton("Create Files");
+		btnOne.setToolTipText("Create Files");
 		btnOne.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				doBtnOne();
@@ -227,8 +272,9 @@ public class TempFiles {
 		btnOne.setMaximumSize(new Dimension(70, 20));
 		btnOne.setPreferredSize(new Dimension(50, 20));
 		toolBar.add(btnOne);
-		
+
 		btnTwo = new JButton("Create/Delete");
+		btnTwo.setToolTipText("Create/Delete");
 		btnTwo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				doBtnTwo();
@@ -237,8 +283,9 @@ public class TempFiles {
 		btnTwo.setPreferredSize(new Dimension(30, 20));
 		btnTwo.setMaximumSize(new Dimension(70, 20));
 		toolBar.add(btnTwo);
-		
-		btnThree = new JButton("3");
+
+		btnThree = new JButton("NewDisk");
+		btnThree.setToolTipText("NewDisk");
 		btnThree.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				doBtnThree();
@@ -247,7 +294,7 @@ public class TempFiles {
 		btnThree.setPreferredSize(new Dimension(30, 20));
 		btnThree.setMaximumSize(new Dimension(70, 20));
 		toolBar.add(btnThree);
-		
+
 		btnFour = new JButton("Env Vars");
 		btnFour.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -257,7 +304,7 @@ public class TempFiles {
 		btnFour.setPreferredSize(new Dimension(30, 20));
 		btnFour.setMaximumSize(new Dimension(70, 20));
 		toolBar.add(btnFour);
-		
+
 		splitPane1 = new JSplitPane();
 		GridBagConstraints gbc_splitPane1 = new GridBagConstraints();
 		gbc_splitPane1.insets = new Insets(0, 0, 5, 0);
@@ -265,36 +312,45 @@ public class TempFiles {
 		gbc_splitPane1.gridx = 0;
 		gbc_splitPane1.gridy = 1;
 		frmTemplate.getContentPane().add(splitPane1, gbc_splitPane1);
-		
+
 		JPanel panelLeft = new JPanel();
 		splitPane1.setLeftComponent(panelLeft);
 		GridBagLayout gbl_panelLeft = new GridBagLayout();
-		gbl_panelLeft.columnWidths = new int[]{0, 0};
-		gbl_panelLeft.rowHeights = new int[]{0, 0};
-		gbl_panelLeft.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gbl_panelLeft.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panelLeft.columnWidths = new int[] { 0, 0 };
+		gbl_panelLeft.rowHeights = new int[] { 0, 0 };
+		gbl_panelLeft.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
+		gbl_panelLeft.rowWeights = new double[] { 1.0, Double.MIN_VALUE };
 		panelLeft.setLayout(gbl_panelLeft);
-		
+
 		JScrollPane scrollPane = new JScrollPane();
 		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
 		gbc_scrollPane.fill = GridBagConstraints.BOTH;
 		gbc_scrollPane.gridx = 0;
 		gbc_scrollPane.gridy = 0;
 		panelLeft.add(scrollPane, gbc_scrollPane);
-		
+
 		textPane = new JTextArea();
+		textPane.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				if (arg0.getClickCount() > 1) {
+					textPane.setText("");
+				}
+
+			}
+		});
 		scrollPane.setViewportView(textPane);
-		
+
 		JPanel panelRight = new JPanel();
 		splitPane1.setRightComponent(panelRight);
 		GridBagLayout gbl_panelRight = new GridBagLayout();
-		gbl_panelRight.columnWidths = new int[]{0};
-		gbl_panelRight.rowHeights = new int[]{0};
-		gbl_panelRight.columnWeights = new double[]{Double.MIN_VALUE};
-		gbl_panelRight.rowWeights = new double[]{Double.MIN_VALUE};
+		gbl_panelRight.columnWidths = new int[] { 0 };
+		gbl_panelRight.rowHeights = new int[] { 0 };
+		gbl_panelRight.columnWeights = new double[] { Double.MIN_VALUE };
+		gbl_panelRight.rowWeights = new double[] { Double.MIN_VALUE };
 		panelRight.setLayout(gbl_panelRight);
 		splitPane1.setDividerLocation(250);
-		
+
 		JPanel panelStatus = new JPanel();
 		panelStatus.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
 		GridBagConstraints gbc_panelStatus = new GridBagConstraints();
@@ -305,10 +361,10 @@ public class TempFiles {
 
 		JMenuBar menuBar = new JMenuBar();
 		frmTemplate.setJMenuBar(menuBar);
-		
+
 		JMenu mnuFile = new JMenu("File");
 		menuBar.add(mnuFile);
-		
+
 		JMenuItem mnuFileNew = new JMenuItem("New");
 		mnuFileNew.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -316,7 +372,7 @@ public class TempFiles {
 			}
 		});
 		mnuFile.add(mnuFileNew);
-		
+
 		JMenuItem mnuFileOpen = new JMenuItem("Open...");
 		mnuFileOpen.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -324,10 +380,10 @@ public class TempFiles {
 			}
 		});
 		mnuFile.add(mnuFileOpen);
-		
+
 		JSeparator separator = new JSeparator();
 		mnuFile.add(separator);
-		
+
 		JMenuItem mnuFileSave = new JMenuItem("Save...");
 		mnuFileSave.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -335,7 +391,7 @@ public class TempFiles {
 			}
 		});
 		mnuFile.add(mnuFileSave);
-		
+
 		JMenuItem mnuFileSaveAs = new JMenuItem("Save As...");
 		mnuFileSaveAs.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -343,10 +399,10 @@ public class TempFiles {
 			}
 		});
 		mnuFile.add(mnuFileSaveAs);
-		
+
 		JSeparator separator_2 = new JSeparator();
 		mnuFile.add(separator_2);
-		
+
 		JMenuItem mnuFilePrint = new JMenuItem("Print...");
 		mnuFilePrint.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -354,11 +410,10 @@ public class TempFiles {
 			}
 		});
 		mnuFile.add(mnuFilePrint);
-		
-		
+
 		JSeparator separator_1 = new JSeparator();
 		mnuFile.add(separator_1);
-		
+
 		JMenuItem mnuFileExit = new JMenuItem("Exit");
 		mnuFileExit.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -366,10 +421,10 @@ public class TempFiles {
 			}
 		});
 		mnuFile.add(mnuFileExit);
-		
+
 		JMenu mnuEdit = new JMenu("Edit");
 		menuBar.add(mnuEdit);
-		
+
 		JMenuItem mnuEditCut = new JMenuItem("Cut");
 		mnuEditCut.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -377,7 +432,7 @@ public class TempFiles {
 			}
 		});
 		mnuEdit.add(mnuEditCut);
-		
+
 		JMenuItem mnuEditCopy = new JMenuItem("Copy");
 		mnuEditCopy.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -385,7 +440,7 @@ public class TempFiles {
 			}
 		});
 		mnuEdit.add(mnuEditCopy);
-		
+
 		JMenuItem mnuEditPaste = new JMenuItem("Paste");
 		mnuEditPaste.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
@@ -394,7 +449,6 @@ public class TempFiles {
 		});
 		mnuEdit.add(mnuEditPaste);
 
-		
 		frmTemplate.addWindowListener(new WindowAdapter() {
 			@Override
 			public void windowClosing(WindowEvent arg0) {
