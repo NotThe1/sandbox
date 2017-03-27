d4feacf3b9decdf302f2b295baafcad357cf29c9
Frank Martyn <FrankMartyn1945@gmail.com>
2017-03-27 11:04:49 -0400

    Replaced with the original GUItemplate

--- sandbox/src/gitTest/GUItemplate.java
+++ sandbox/src/gitTest/GUItemplate.java
@@ -8,322 +8,20 @@ import java.awt.Insets;
 import java.awt.Point;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-//public class GUItemplate {
-//
-//	private JFrame frmTemplate;
-//	private JButton btnOne;
-//	private JButton btnTwo;
-//	private JButton btnThree;
-//	private JButton btnFour;
-//	private JSplitPane splitPane1;
-//
-//	/**
-//	 * Launch the application.
-//	 */
-//	public static void main(String[] args) {
-//		EventQueue.invokeLater(new Runnable() {
-//			public void run() {
-//				try {
-//					GUItemplate window = new GUItemplate();
-//					window.frmTemplate.setVisible(true);
-//				} catch (Exception e) {
-//					e.printStackTrace();
-//				}//try
-//			}//run
-//		});
-//	}// main
-//
-//	/* Standard Stuff */
-//	
-//	private void doBtnOne(){
-//		
-//	}//doBtnOne
-//	
-//	private void doBtnTwo(){
-//		
-//	}//doBtnTwo
-//	
-//	private void doBtnThree(){
-//		
-//	}//doBtnThree
-//	
-//	private void doBtnFour(){
-//		
-//	}//doBtnFour
-//	
-//	//---------------------------------------------------------
-//	
-//	private void doFileNew(){
-//		
-//	}//doFileNew
-//	private void doFileOpen(){
-//		
-//	}//doFileOpen
-//	private void doFileSave(){
-//		
-//	}//doFileSave
-//	private void doFileSaveAs(){
-//		
-//	}//doFileSaveAs
-//	private void doFilePrint(){
-//		
-//	}//doFilePrint
-//	private void doFileExit(){
-//		appClose();
-//		System.exit(0);
-//	}//doFileExit
-//	private void doEditCut(){
-//		
-//	}//doEditCut
-//	private void doEditCopy(){
-//		
-//	}//doEditCopy
-//	private void doEditPaste(){
-//		
-//	}//doEditPaste
-//
-//	private void appClose() {
-//		Preferences myPrefs = Preferences.userNodeForPackage(GUItemplate.class);
-//		Dimension dim = frmTemplate.getSize();
-//		myPrefs.putInt("Height", dim.height);
-//		myPrefs.putInt("Width", dim.width);
-//		Point point = frmTemplate.getLocation();
-//		myPrefs.putInt("LocX", point.x);
-//		myPrefs.putInt("LocY", point.y);
-//		myPrefs.putInt("Divider", splitPane1.getDividerLocation());
-//		myPrefs = null;
-//	}//appClose
-//
-//	private void appInit() {
-//		Preferences myPrefs = Preferences.userNodeForPackage(GUItemplate.class);
-//		frmTemplate.setSize(myPrefs.getInt("Width", 500), myPrefs.getInt("Height", 500));
-//		frmTemplate.setLocation(myPrefs.getInt("LocX", 100), myPrefs.getInt("LocY", 100));
-//		splitPane1.setDividerLocation(myPrefs.getInt("Divider", 250));
-//		myPrefs = null;
-//	}// appInit
-//
-//	public GUItemplate() {
-//		initialize();
-//		appInit();
-//	}// Constructor
-//
-//	/**
-//	 * Initialize the contents of the frame.
-//	 */
-//	private void initialize() {
-//		frmTemplate = new JFrame();
-//		frmTemplate.setTitle("GUItemplate");
-//		frmTemplate.setBounds(100, 100, 450, 300);
-//		frmTemplate.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-//		frmTemplate.addWindowListener(new WindowAdapter() {
-//			@Override
-//			public void windowClosing(WindowEvent arg0) {
-//				appClose();
-//			}
-//		});
-//		GridBagLayout gridBagLayout = new GridBagLayout();
-//		gridBagLayout.columnWidths = new int[] { 0, 0 };
-//		gridBagLayout.rowHeights = new int[] { 0, 0, 25, 0 };
-//		gridBagLayout.columnWeights = new double[] { 1.0, Double.MIN_VALUE };
-//		gridBagLayout.rowWeights = new double[] { 0.0, 1.0, 0.0, Double.MIN_VALUE };
-//		frmTemplate.getContentPane().setLayout(gridBagLayout);
-//		
-//		JToolBar toolBar = new JToolBar();
-//		toolBar.setFloatable(false);
-//		GridBagConstraints gbc_toolBar = new GridBagConstraints();
-//		gbc_toolBar.fill = GridBagConstraints.HORIZONTAL;
-//		gbc_toolBar.insets = new Insets(0, 0, 5, 0);
-//		gbc_toolBar.gridx = 0;
-//		gbc_toolBar.gridy = 0;
-//		frmTemplate.getContentPane().add(toolBar, gbc_toolBar);
-//		
-//		btnOne = new JButton("1");
-//		btnOne.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent arg0) {
-//				doBtnOne();
-//			}
-//		});
-//		btnOne.setMaximumSize(new Dimension(70, 20));
-//		btnOne.setPreferredSize(new Dimension(50, 20));
-//		toolBar.add(btnOne);
-//		
-//		btnTwo = new JButton("2");
-//		btnTwo.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//				doBtnTwo();
-//			}
-//		});
-//		btnTwo.setPreferredSize(new Dimension(30, 20));
-//		btnTwo.setMaximumSize(new Dimension(70, 20));
-//		toolBar.add(btnTwo);
-//		
-//		btnThree = new JButton("3");
-//		btnThree.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//				doBtnThree();
-//			}
-//		});
-//		btnThree.setPreferredSize(new Dimension(30, 20));
-//		btnThree.setMaximumSize(new Dimension(70, 20));
-//		toolBar.add(btnThree);
-//		
-//		btnFour = new JButton("4");
-//		btnFour.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent e) {
-//				doBtnFour();
-//			}
-//		});
-//		btnFour.setPreferredSize(new Dimension(30, 20));
-//		btnFour.setMaximumSize(new Dimension(70, 20));
-//		toolBar.add(btnFour);
-//		
-//		splitPane1 = new JSplitPane();
-//		GridBagConstraints gbc_splitPane1 = new GridBagConstraints();
-//		gbc_splitPane1.insets = new Insets(0, 0, 5, 0);
-//		gbc_splitPane1.fill = GridBagConstraints.BOTH;
-//		gbc_splitPane1.gridx = 0;
-//		gbc_splitPane1.gridy = 1;
-//		frmTemplate.getContentPane().add(splitPane1, gbc_splitPane1);
-//		
-//		JPanel panelLeft = new JPanel();
-//		splitPane1.setLeftComponent(panelLeft);
-//		GridBagLayout gbl_panelLeft = new GridBagLayout();
-//		gbl_panelLeft.columnWidths = new int[]{0};
-//		gbl_panelLeft.rowHeights = new int[]{0};
-//		gbl_panelLeft.columnWeights = new double[]{Double.MIN_VALUE};
-//		gbl_panelLeft.rowWeights = new double[]{Double.MIN_VALUE};
-//		panelLeft.setLayout(gbl_panelLeft);
-//		
-//		JPanel panelRight = new JPanel();
-//		splitPane1.setRightComponent(panelRight);
-//		GridBagLayout gbl_panelRight = new GridBagLayout();
-//		gbl_panelRight.columnWidths = new int[]{0};
-//		gbl_panelRight.rowHeights = new int[]{0};
-//		gbl_panelRight.columnWeights = new double[]{Double.MIN_VALUE};
-//		gbl_panelRight.rowWeights = new double[]{Double.MIN_VALUE};
-//		panelRight.setLayout(gbl_panelRight);
-//		splitPane1.setDividerLocation(250);
-//		
-//		JPanel panelStatus = new JPanel();
-//		panelStatus.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
-//		GridBagConstraints gbc_panelStatus = new GridBagConstraints();
-//		gbc_panelStatus.fill = GridBagConstraints.BOTH;
-//		gbc_panelStatus.gridx = 0;
-//		gbc_panelStatus.gridy = 2;
-//		frmTemplate.getContentPane().add(panelStatus, gbc_panelStatus);
-//
-//		JMenuBar menuBar = new JMenuBar();
-//		frmTemplate.setJMenuBar(menuBar);
-//		
-//		JMenu mnuFile = new JMenu("File");
-//		menuBar.add(mnuFile);
-//		
-//		JMenuItem mnuFileNew = new JMenuItem("New");
-//		mnuFileNew.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent arg0) {
-//				doFileNew();
-//			}
-//		});
-//		mnuFile.add(mnuFileNew);
-//		
-//		JMenuItem mnuFileOpen = new JMenuItem("Open...");
-//		mnuFileOpen.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent arg0) {
-//				doFileOpen();
-//			}
-//		});
-//		mnuFile.add(mnuFileOpen);
-//		
-//		JSeparator separator = new JSeparator();
-//		mnuFile.add(separator);
-//		
-//		JMenuItem mnuFileSave = new JMenuItem("Save...");
-//		mnuFileSave.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent arg0) {
-//				doFileSave();
-//			}
-//		});
-//		mnuFile.add(mnuFileSave);
-//		
-//		JMenuItem mnuFileSaveAs = new JMenuItem("Save As...");
-//		mnuFileSaveAs.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent arg0) {
-//				doFileSaveAs();
-//			}
-//		});
-//		mnuFile.add(mnuFileSaveAs);
-//		
-//		JSeparator separator_2 = new JSeparator();
-//		mnuFile.add(separator_2);
-//		
-//		JMenuItem mnuFilePrint = new JMenuItem("Print...");
-//		mnuFilePrint.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent arg0) {
-//				doFilePrint();
-//			}
-//		});
-//		mnuFile.add(mnuFilePrint);
-//		
-//		
-//		JSeparator separator_1 = new JSeparator();
-//		mnuFile.add(separator_1);
-//		
-//		JMenuItem mnuFileExit = new JMenuItem("Exit");
-//		mnuFileExit.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent arg0) {
-//				doFileExit();
-//			}
-//		});
-//		mnuFile.add(mnuFileExit);
-//		
-//		JMenu mnuEdit = new JMenu("Edit");
-//		menuBar.add(mnuEdit);
-//		
-//		JMenuItem mnuEditCut = new JMenuItem("Cut");
-//		mnuEditCut.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent arg0) {
-//				doEditCut();
-//			}
-//		});
-//		mnuEdit.add(mnuEditCut);
-//		
-//		JMenuItem mnuEditCopy = new JMenuItem("Copy");
-//		mnuEditCopy.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent arg0) {
-//				doEditCopy();
-//			}
-//		});
-//		mnuEdit.add(mnuEditCopy);
-//		
-//		JMenuItem mnuEditPaste = new JMenuItem("Paste");
-//		mnuEditPaste.addActionListener(new ActionListener() {
-//			public void actionPerformed(ActionEvent arg0) {
-//				doEditPaste();
-//			}
-//		});
-//		mnuEdit.add(mnuEditPaste);
-//=======
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-//<<<<<<< HEAD
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.util.prefs.Preferences;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
-import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
 import javax.swing.JSplitPane;
-import javax.swing.JTextArea;
-import javax.swing.SwingConstants;
+import javax.swing.JToolBar;
 import javax.swing.border.BevelBorder;
-import javax.swing.text.JTextComponent;
 
 public class GUItemplate {
 
@@ -333,8 +31,6 @@ public class GUItemplate {
 	private JButton btnThree;
 	private JButton btnFour;
 	private JSplitPane splitPane1;
-	private JTextArea txtLog;
-	private JLabel lblLog;
 
 	/**
 	 * Launch the application.
@@ -391,7 +87,15 @@ public class GUItemplate {
 		appClose();
 		System.exit(0);
 	}//doFileExit
-
+	private void doEditCut(){
+		
+	}//doEditCut
+	private void doEditCopy(){
+		
+	}//doEditCopy
+	private void doEditPaste(){
+		
+	}//doEditPaste
 
 	private void appClose() {
 		Preferences myPrefs = Preferences.userNodeForPackage(GUItemplate.class);
@@ -407,11 +111,10 @@ public class GUItemplate {
 
 	private void appInit() {
 		Preferences myPrefs = Preferences.userNodeForPackage(GUItemplate.class);
-		frmTemplate.setSize(myPrefs.getInt("Width", 761), myPrefs.getInt("Height", 693));
+		frmTemplate.setSize(myPrefs.getInt("Width", 500), myPrefs.getInt("Height", 500));
 		frmTemplate.setLocation(myPrefs.getInt("LocX", 100), myPrefs.getInt("LocY", 100));
 		splitPane1.setDividerLocation(myPrefs.getInt("Divider", 250));
 		myPrefs = null;
-		txtLog.append(" this is the log");
 	}// appInit
 
 	public GUItemplate() {
@@ -440,79 +143,54 @@ public class GUItemplate {
 		gridBagLayout.rowWeights = new double[] { 0.0, 1.0, 0.0, Double.MIN_VALUE };
 		frmTemplate.getContentPane().setLayout(gridBagLayout);
 		
-		JPanel panel = new JPanel();
-		GridBagConstraints gbc_panel = new GridBagConstraints();
-		gbc_panel.insets = new Insets(0, 0, 5, 0);
-		gbc_panel.fill = GridBagConstraints.BOTH;
-		gbc_panel.gridx = 0;
-		gbc_panel.gridy = 0;
-		frmTemplate.getContentPane().add(panel, gbc_panel);
-		GridBagLayout gbl_panel = new GridBagLayout();
-		gbl_panel.columnWidths = new int[]{0, 0, 0, 0, 0};
-		gbl_panel.rowHeights = new int[]{0, 0};
-		gbl_panel.columnWeights = new double[]{0.0, 0.0, 0.0, 0.0, Double.MIN_VALUE};
-		gbl_panel.rowWeights = new double[]{0.0, Double.MIN_VALUE};
-		panel.setLayout(gbl_panel);
+		JToolBar toolBar = new JToolBar();
+		toolBar.setFloatable(false);
+		GridBagConstraints gbc_toolBar = new GridBagConstraints();
+		gbc_toolBar.fill = GridBagConstraints.HORIZONTAL;
+		gbc_toolBar.insets = new Insets(0, 0, 5, 0);
+		gbc_toolBar.gridx = 0;
+		gbc_toolBar.gridy = 0;
+		frmTemplate.getContentPane().add(toolBar, gbc_toolBar);
 		
-		btnOne = new JButton("Button 1");
-		btnOne.setMinimumSize(new Dimension(100, 20));
-		GridBagConstraints gbc_btnOne = new GridBagConstraints();
-		gbc_btnOne.insets = new Insets(0, 0, 0, 5);
-		gbc_btnOne.gridx = 0;
-		gbc_btnOne.gridy = 0;
-		panel.add(btnOne, gbc_btnOne);
+		btnOne = new JButton("1");
 		btnOne.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				doBtnOne();
 			}
 		});
-		btnOne.setMaximumSize(new Dimension(0, 0));
-		btnOne.setPreferredSize(new Dimension(100, 20));
+		btnOne.setMaximumSize(new Dimension(70, 20));
+		btnOne.setPreferredSize(new Dimension(50, 20));
+		toolBar.add(btnOne);
 		
-		btnTwo = new JButton("Button 2");
-		btnTwo.setMinimumSize(new Dimension(100, 20));
-		GridBagConstraints gbc_btnTwo = new GridBagConstraints();
-		gbc_btnTwo.insets = new Insets(0, 0, 0, 5);
-		gbc_btnTwo.gridx = 1;
-		gbc_btnTwo.gridy = 0;
-		panel.add(btnTwo, gbc_btnTwo);
+		btnTwo = new JButton("2");
 		btnTwo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				doBtnTwo();
 			}
 		});
-		btnTwo.setPreferredSize(new Dimension(100, 20));
-		btnTwo.setMaximumSize(new Dimension(0, 0));
+		btnTwo.setPreferredSize(new Dimension(30, 20));
+		btnTwo.setMaximumSize(new Dimension(70, 20));
+		toolBar.add(btnTwo);
 		
-		btnThree = new JButton("Button 3");
-		btnThree.setMinimumSize(new Dimension(100, 20));
-		GridBagConstraints gbc_btnThree = new GridBagConstraints();
-		gbc_btnThree.insets = new Insets(0, 0, 0, 5);
-		gbc_btnThree.gridx = 2;
-		gbc_btnThree.gridy = 0;
-		panel.add(btnThree, gbc_btnThree);
+		btnThree = new JButton("3");
 		btnThree.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				doBtnThree();
 			}
 		});
-		btnThree.setPreferredSize(new Dimension(100, 20));
-		btnThree.setMaximumSize(new Dimension(0, 0));
+		btnThree.setPreferredSize(new Dimension(30, 20));
+		btnThree.setMaximumSize(new Dimension(70, 20));
+		toolBar.add(btnThree);
 		
-		btnFour = new JButton("Button 4");
-		btnFour.setMinimumSize(new Dimension(100, 20));
-		GridBagConstraints gbc_btnFour = new GridBagConstraints();
-		gbc_btnFour.anchor = GridBagConstraints.NORTH;
-		gbc_btnFour.gridx = 3;
-		gbc_btnFour.gridy = 0;
-		panel.add(btnFour, gbc_btnFour);
+		btnFour = new JButton("4");
 		btnFour.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				doBtnFour();
 			}
 		});
-		btnFour.setPreferredSize(new Dimension(100, 20));
-		btnFour.setMaximumSize(new Dimension(0, 0));
+		btnFour.setPreferredSize(new Dimension(30, 20));
+		btnFour.setMaximumSize(new Dimension(70, 20));
+		toolBar.add(btnFour);
 		
 		splitPane1 = new JSplitPane();
 		GridBagConstraints gbc_splitPane1 = new GridBagConstraints();
@@ -534,34 +212,11 @@ public class GUItemplate {
 		JPanel panelRight = new JPanel();
 		splitPane1.setRightComponent(panelRight);
 		GridBagLayout gbl_panelRight = new GridBagLayout();
-		gbl_panelRight.columnWidths = new int[]{0, 0};
-		gbl_panelRight.rowHeights = new int[]{0, 0};
-		gbl_panelRight.columnWeights = new double[]{1.0, Double.MIN_VALUE};
-		gbl_panelRight.rowWeights = new double[]{1.0, Double.MIN_VALUE};
+		gbl_panelRight.columnWidths = new int[]{0};
+		gbl_panelRight.rowHeights = new int[]{0};
+		gbl_panelRight.columnWeights = new double[]{Double.MIN_VALUE};
+		gbl_panelRight.rowWeights = new double[]{Double.MIN_VALUE};
 		panelRight.setLayout(gbl_panelRight);
-		
-		JScrollPane scrollPane = new JScrollPane();
-		GridBagConstraints gbc_scrollPane = new GridBagConstraints();
-		gbc_scrollPane.fill = GridBagConstraints.BOTH;
-		gbc_scrollPane.gridx = 0;
-		gbc_scrollPane.gridy = 0;
-		panelRight.add(scrollPane, gbc_scrollPane);
-		
-		txtLog = new JTextArea();
-		txtLog.addMouseListener(new MouseAdapter() {
-			@Override
-			public void mouseClicked(MouseEvent arg0) {
-				if (arg0.getClickCount()>1){
-					((JTextComponent) arg0.getComponent()).setText("");
-					txtLog.setText("");
-				}//if
-			}//mouseClicked
-		});
-		scrollPane.setViewportView(txtLog);
-		
-		lblLog = new JLabel("New label");
-		lblLog.setHorizontalAlignment(SwingConstants.CENTER);
-		scrollPane.setColumnHeaderView(lblLog);
 		splitPane1.setDividerLocation(250);
 		
 		JPanel panelStatus = new JPanel();
@@ -635,7 +290,33 @@ public class GUItemplate {
 			}
 		});
 		mnuFile.add(mnuFileExit);
-//>>>>>>> refs/heads/trimMenus
+		
+		JMenu mnuEdit = new JMenu("Edit");
+		menuBar.add(mnuEdit);
+		
+		JMenuItem mnuEditCut = new JMenuItem("Cut");
+		mnuEditCut.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				doEditCut();
+			}
+		});
+		mnuEdit.add(mnuEditCut);
+		
+		JMenuItem mnuEditCopy = new JMenuItem("Copy");
+		mnuEditCopy.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				doEditCopy();
+			}
+		});
+		mnuEdit.add(mnuEditCopy);
+		
+		JMenuItem mnuEditPaste = new JMenuItem("Paste");
+		mnuEditPaste.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				doEditPaste();
+			}
+		});
+		mnuEdit.add(mnuEditPaste);
 
 		
 
