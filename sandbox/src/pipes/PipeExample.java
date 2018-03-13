68107a6e828a46269513ec908b8c0b687cf83d64
Frank Martyn <FrankMartyn1945@gmail.com>
2018-03-13 13:14:06 -0400

    Playing with pipes

---
+++ sandbox/src/pipes/PipeExample.java
@@ -0,0 +1,46 @@
+package pipes;
+
+import java.io.IOException;
+import java.io.PipedInputStream;
+import java.io.PipedOutputStream;
+
+public class PipeExample {
+
+	public static void main(String[] args) throws IOException{
+
+		final PipedOutputStream output = new PipedOutputStream();
+		final PipedInputStream input = new PipedInputStream(output);
+
+		Thread thread1 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				try {
+					output.write("Hello world, pipe!".getBytes());
+				} catch (IOException e) {
+					e.getMessage();
+				} // try
+			}// run
+		});// thread1
+		
+		Thread thread2 = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				int a = 1;
+				try {
+					int data = input.read();
+					while(data!=-1) {
+						System.out.print((char) data);
+						data = input.read();
+					}//
+				} catch (IOException e) {
+					e.getMessage();
+				}
+			}//run
+		});//thread2
+		
+		thread1.start();
+		thread2.start();
+
+	}// main
+
+}// class PipeExample
